ccc502a1be468249ba4e7af03dc4646b1d086506
dxmai <19521820@gm.uit.edu.vn>
2022-05-09 01:18:33 +0700

    modified app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import os
 from PIL import Image
+import time
+from helper import *
 
 st.set_page_config(page_title='Image Search', page_icon=':frame_with_picture')
 st.title("Hệ thống tìm kiếm hình ảnh :sunny:")
@@ -11,6 +12,11 @@ uploaded_image = st.sidebar.file_uploader("")
 st.sidebar.write("Hoặc chụp hình ảnh :camera_with_flash:")
 picture = st.sidebar.camera_input("")
 
+top = st.sidebar.selectbox(
+     'Lựa chọn số lượng hình ảnh trả về:',
+     (10, 20, 50, 100, 200, 1000000000))
+st.sidebar.write("1000000000 ~ Tất cả hình ảnh")
+
 run_model = st.sidebar.button("Tìm kiếm")
 flag = False
 
@@ -20,12 +26,34 @@ if run_model:
     else:
         flag = True
 
+with open("imagespath.pkl", "rb") as file:
+    image_paths = pickle.load(file)
+data_feature = np.load('colorthief_5.npy')
+top = top if top != 1000000000 else len(data_feature)
+
 if flag:
-    st.write(":tada:")
+    img_path = uploaded_image if uploaded_image is not None else picture
+    query_img = Image.open(img_path)
+    query_img = query_img.resize((250, 300), Image.ANTIALIAS)
+    st.image(query_img, "Ảnh tải lên")
+    start_time = time.time()
+    with st.spinner("Xin vui lòng chờ một chút..."):
+        query_arr = np.array(query_img)
+        cosine_arr = CalculateCosine(query_arr, data_feature)
+        top_indices = cosine_arr.argsort()[:-(top+1):-1]
+        top_paths = [image_paths[i] for i in top_indices]
+    st.success("Tìm kiếm hoàn tất! :tada:")
+    end_time = time.time()
+    st.write(":stopwatch: Top 20 kết quả trả về trong ", round(end_time - start_time, 4), "s")
     col = st.columns(4)
-    image_path = os.getcwd() + '/dataset/sunrise/'
-    l = os.listdir(image_path)
-    for i in range(20):
-        image = Image.open(image_path + l[i])
+    paths = []
+    for i in range(top):
+        res_path = top_paths[i]
+        res_path = res_path.replace('/gdrive/MyDrive/', '\\')
+        res_path = res_path.replace('/', '\\')
+        image_path = os.getcwd() + res_path
+        paths.append(image_path)
+    for i in range(top):
+        image = Image.open(paths[i])
         image = image.resize((250, 300), Image.ANTIALIAS)
         col[i % 4].image(image)
